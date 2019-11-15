359b79178b84a05d2d69816d40f8caf5cada57c3
laury <laury46@gmail.com>
2019-11-15 18:38:38 +0100

    ex1

--- Ejercicio1/ejercicio1.ts
+++ Ejercicio1/ejercicio1.ts
@@ -2,15 +2,34 @@
 
 Apartado A
 
-Dado un array multidimensional, construye una función inmutable que devuelva el mismo array aplanado, esto
-es, con un único nivel de profundidad. Por ejemplo, el siguiente array:
+Dado un array multidimensional, construye una función inmutable que devuelva el 
+mismo array aplanado, esto es, con un único nivel de profundidad. 
+Por ejemplo, el siguiente array:
+
 const sample = [1, [2, 3], [[4], [5, 6, [7, 8, [9]]]]];
+
 quedaría aplanado como:
 // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
 
+const sample = [1, [2, 3], [[4], [5, 6, [7, 8, [9]]]]];
+
+
+function buildNewFlatArray(sample) {
+   return sample.reduce((acc, val) => Array.isArray(val) ? 
+        acc.concat(buildNewFlatArray(val)) : acc.concat(val), []);
+}
+
+buildNewFlatArray(sample); 
+
+
+
+/*
 Apartado B
 
-¿Has resuelto el ejercicio anterior? Suponiendo que los arrays multidimensionales del ejercicio anterior no
-serán de naturaleza mixta, es decir, sus elementos siempre serán del mismo tipo ¿Serías capaz de proporcionar
+¿Has resuelto el ejercicio anterior? Suponiendo que los arrays multidimensionales
+ del ejercicio anterior no
+serán de naturaleza mixta, es decir, sus elementos siempre serán del mismo 
+tipo ¿Serías capaz de proporcionar
 un tipado adecuado a dicha función de aplanamiento?
 */
--- src/playground.ts
+++ src/playground.ts
@@ -1,2 +1,11 @@
 console.log("playgroundTS");
 
+const sample = [1, [2, 3], [[4], [5, 6, [7, 8, [9]]]]];
+
+
+function buildNewFlatArray(sample) {
+   return sample.reduce((acc, val) => Array.isArray(val) ? 
+        acc.concat(buildNewFlatArray(val)) : acc.concat(val), []);
+}
+
+buildNewFlatArray(sample); 
